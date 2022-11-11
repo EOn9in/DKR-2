58a531151bd7d5f73848b7f22946479778ad3716
伊凡 <117629350+EOn9in@users.noreply.github.com>
2022-11-11 12:15:38 +0300

    Add files via upload

---
+++ ДКР №2/Z1.pas
@@ -0,0 +1,31 @@
+﻿var a,b:array of integer;
+begin
+var n := ReadInteger('Введите длину массива:');
+print('Введите элементы массива');
+println;
+SetLength(a,n);
+for var i:=0 to n-1 do
+ begin
+  print('a[',i+1,']=');
+  a[i]:=ReadInteger;
+ end;
+ print(a);
+ println;
+ var d := ReadInteger('Введите длину последовательности для поиска от 2 до 10  d = ');
+if d>n then print('Последовательность для поиска не может быть больше самого массива')
+else
+println('Введите элементы последовательности для поиска');
+SetLength(b,d);
+for var i:=0 to d-1 do
+b[i]:=ReadInteger;
+var i:=0;
+while (i<n)and(a[i]<>b[1]) do inc(i);
+if i=n then write('Последовательности в массиве нет')
+else
+ begin
+   var j:=i;
+  while(j<=d)and(b[j]=a[j]) do inc(j);
+  if j<=d then print('Последовательности в массиве нет')
+   else print('Последовательность в массиве есть')
+ end;
+end.
---
+++ ДКР №2/Zadanie 2.pas
@@ -0,0 +1,41 @@
+﻿var a: array[1..100] of string; n: array[1..100] of integer;
+begin
+  var i := 0;var j := 0;var m := 1;var t := '';
+  var s:= ReadString('Введите строку:');
+  for i := 1 to length(s) do if s[i] <> ' ' then t := t + s[i] else begin
+  if t <> '' then
+    for var k := 1 to j do 
+      if a[k] = t then begin
+        n[k] := n[k] + 1;
+        if n[m] < n[k] then m := k;
+        t := '';
+        break;
+      end;
+  if t <> '' then begin
+    j := j + 1;
+    n[j] := 1;
+    a[j] := t;
+    t := ''
+  end;
+end;
+begin
+  if t <> '' then
+    for var k := 1 to j do 
+      if a[k] = t then begin
+        n[k] := n[k] + 1;
+        if n[m] < n[k] then m := k;
+        t := '';
+        break;
+      end;
+  if t <> '' then begin
+    j := j + 1;
+    n[j] := 1;
+    a[j] := t;
+    t := ''
+  end;
+end;
+  for i := 1 to j do 
+  begin
+    if n[i] = n[m] then print(a[i]);
+  end;
+end.
